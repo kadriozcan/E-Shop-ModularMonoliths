a2798a4f6d60c94711f7af12f2e082a5faa4f235
kadriozcan <kadri.ozcan@outlook.com>
2025-01-26 19:45:38 +0300

    Database config

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Data;
 
 namespace Catalog;
 
@@ -9,16 +10,34 @@ public static class CatalogModule
     public static IServiceCollection AddCatalogModule(this IServiceCollection services,
         IConfiguration configuration)
     {
-        //
+        // Add services to the DI container
+
+
+        // 1. Api Endpoint services
+
+        // 2. Application Use Case services
+
+        // 3. Data - Infrastructure services
+        var connectionString = configuration.GetConnectionString("Database");
+        services.AddDbContext<CatalogDbContext>(options =>
+            options.UseNpgsql(connectionString));
 
         return services;
     }
 
     public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
     {
-        //
+        // Configure HTTP request pipeline
+
+
+        // 1. Use Api Endpoint services
+
+        // 2. Use Application Use Case Services
+
+        // 3. Use Data - Infrastructure services
+        app.UseMigration<CatalogDbContext>();
+
 
         return app;
     }
 }
-
